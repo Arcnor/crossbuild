d40bdf07596275f9524a9cbcb09b157dc57308a0
Malik Enes Safak <e.maliksafak@gmail.com>
2020-05-21 22:27:28 +0300

    fix image name

--- scripts/crosscompile.ps1
+++ scripts/crosscompile.ps1
@@ -34,7 +34,7 @@
 	if($var){
 		$CURRENTDIR = (Get-Location).Path
 		$Command = "docker"
-		$Parms = "run -it --rm -v ${CURRENTDIR}:/workspace -w /workspace -e CROSS_TRIPLE=${var} multiarch/crossbuild:buster"
+		$Parms = "run -it --rm -v ${CURRENTDIR}:/workspace -w /workspace -e CROSS_TRIPLE=${var} multiarch/crossbuild:dev"
 		$Prms = $Parms.Split(" ")
 		Start-Process "$Command" $Prms
 	}
